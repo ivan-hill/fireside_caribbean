8946acd8bd578bfa41411856b82bd82bd74fc4fa
Ivan Hill <30849044+ivan-hill@users.noreply.github.com>
2021-09-10 08:02:41 +0200

    Update index.js

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -16,12 +16,12 @@ const Sidebar = ({ isOpen, toggle }) => {
         <CloseIcon />
       </Icon>
       <SidebarMenu>
-        <SidebarLink to='/Products/index' onClick={toggle}>Main</SidebarLink>
-        <SidebarLink to='/'>Side Dishes</SidebarLink>
-        <SidebarLink to='/'>Full Menu</SidebarLink>
+        <SidebarLink to='/Products/index' onClick={toggle}>Address: 796 Penn Ave, Pittsburgh, PA 15221</SidebarLink>
+        <SidebarLink to='/'>Phone: +1 412-243-6344</SidebarLink>
+        <SidebarLink to='/'>Hours: 11am - 7pm Daily</SidebarLink>
       </SidebarMenu>
       <SideBtnWrap>
-        <SidebarRoute to='/'>Order Now</SidebarRoute>
+        <SidebarRoute to='/'>Cash Only</SidebarRoute>
       </SideBtnWrap>
     </SidebarContainer>
   );
